a45280f3e63859ed831e0d61b9f82b559523bd55
nopenoway0 <nopenoway0>
2017-05-04 02:46:55 -0700

    name change more functions. Pickleable now

--- c_python_wrapper/Engine_Info.cpp
+++ c_python_wrapper/Engine_Info.cpp
@@ -5,20 +5,34 @@
 #include <boost/python.hpp>
 #include <vector>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include <socket_functions.h>
 
-BOOST_PYTHON_MODULE(engine_info)
+BOOST_PYTHON_MODULE(shmaro)
 {
 	using namespace boost::python;
+
+	def("start_server", start_server);
+	def("stop_server", stop_server);
+	def ("get_list", get_list);
+
 	class_<error >("error_struct")
 	.def_readonly("name", &error::name)
 	.def_readwrite("number", &error::number)
 	;
+
+	class_<std::vector<Engine_Info> >("engine_info_vector")
+	.def(vector_indexing_suite<std::vector<Engine_Info> >())
+	;
+
 	class_<std::vector<error> >("error_vector")
 	.def(vector_indexing_suite<std::vector<error> >())
 	;
-	class_<Engine_Info>("Engine_Info", init<int, double>())
+
+	class_<Engine_Info>("Engine_Info", init<int, double, int>())
 	.def("get_rpm", &Engine_Info::get_rpm)
 	.def("get_load", &Engine_Info::get_load)
 	.def("get_errors", &Engine_Info::get_errors)
+	.def("get_flag", &Engine_Info::get_flag)
+	.def_pickle(engine_info_pickle_suite())
 	;
 }
